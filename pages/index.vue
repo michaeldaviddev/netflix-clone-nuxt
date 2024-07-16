4929ca3c5b6b5e66ccf3b5354865300ddb75c8c2
Michael David Machacuay <mdavidmachacuay@gmail.com>
2024-07-16 18:13:30 -0500

    create index page

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div class="slider">
+      <div class="slider-item">
+        <img src="https://occ-0-2645-1123.1.nflxso.net/dnm/api/v6/Qs00mKCpRvrkl3HZAN5KwEL1kpE/AAAABXOvVJZdRnu3EU1Nz3yrF48GpN85jRjJXwSo1uNHsmZko8rhNXcKgA-dl9XofUCZdy9WZ905Nz-CooQP7pq_FjA8dbSQ1TnUlcXdvNkJrvabyb4kVMeW9oM6n34dYPj6J32m.jpg?r=a95" alt="">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
